6a2071772da7826d815648e928e2d0dfeab70f1a
zhangsiwei <zhangsiwei@qq.com>
2020-09-18 16:34:02 +0800

    1、增加系统操作日志界面 2、增加系统异常日志界面

--- src/api/base/base.js
+++ src/api/base/base.js
@@ -1,10 +1,21 @@
 import request from '@/utils/request'
 const baseURL = '/base'
 
-export function listByPage(data) {
+export function listByPageErrorLog(data) {
   return request({
     baseURL: baseURL,
     url: '/logSysErrorController/listByPage',
+    method: 'post',
+    // params 以formData格式传递  后台用RequestParam
+    // 直接加data以json格式传递  后台用RequestBody
+    data
+  })
+}
+
+export function listByPageOperationLog(data) {
+  return request({
+    baseURL: baseURL,
+    url: '/logSysActiController/listByPage',
     method: 'post',
     // params 以formData格式传递  后台用RequestParam
     // 直接加data以json格式传递  后台用RequestBody
