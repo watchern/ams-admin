b26483c446cb09d14a710f5df0340a6385b29b88
duyiran <2192156580@qq.com>
2020-09-25 17:54:17 +0800

    --

---
+++ src/views/modules/etlscheduler/dag/definitionDetails.vue
@@ -0,0 +1,96 @@
+<template>
+  <div class="home-main index-model">
+    <m-dag
+      v-if="!isLoading"
+      :type="'definition'"
+      :release-state="releaseState"
+    />
+    <m-spin :is-spin="isLoading" />
+  </div>
+</template>
+<script>
+import mDag from './_source/dag.vue'
+import mSpin from '@/components/Dolphin/spin/spin'
+import Affirm from './_source/jumpAffirm'
+import disabledState from '@/components/Dolphin/mixin/disabledState'
+import { mapActions, mapMutations } from 'vuex'
+
+export default {
+  name: 'DefinitionDetails',
+  mixins: [disabledState],
+  props: {},
+  data() {
+    return {
+      // loading
+      isLoading: true,
+      // state
+      releaseState: ''
+    }
+  },
+  methods: {
+    ...mapMutations('dag', ['resetParams', 'setIsDetails']),
+    ...mapActions('dag', ['getProcessList', 'getProcessDetails']),
+    // ...mapActions('dag', ['getProcessList', 'getProjectList', 'getResourcesList', 'getProcessDetails', 'getResourcesListJar']),
+    // ...mapActions('security', ['getTenantList', 'getWorkerGroupsAll']),
+    /**
+     * init
+     */
+    init() {
+      this.isLoading = true
+      // Initialization parameters
+      this.resetParams()
+      // Promise Get node needs data
+      Promise.all([
+        // Node details
+        this.getProcessDetails(this.$route.params.id),
+        // get process definition
+        this.getProcessList()
+        // get project
+        // this.getProjectList(),
+        // get resource
+        // this.getResourcesList(),
+        // get jar
+        // this.getResourcesListJar()
+        // ,
+        // get worker group list
+        // this.getWorkerGroupsAll(),
+        // this.getTenantList()
+      ]).then((data) => {
+        const item = data[0]
+        this.setIsDetails(item.releaseState === 'ONLINE')
+        this.releaseState = item.releaseState
+        this.isLoading = false
+        // Whether to pop up the box?
+        Affirm.init(this.$root)
+      }).catch(() => {
+        this.isLoading = false
+      })
+    },
+    /**
+     * Redraw (refresh operation)
+     */
+    _reset() {
+      this.getProcessDetails(this.$route.params.id).then(res => {
+        const item = res
+        this.setIsDetails(item.releaseState === 'ONLINE')
+        this.releaseState = item.releaseState
+      })
+    }
+  },
+  watch: {
+    // Listening for routing changes
+    '$route': {
+      deep: true,
+      handler() {
+        this.init()
+      }
+    }
+  },
+  created() {
+    this.init()
+  },
+  mounted() {
+  },
+  components: { mDag, mSpin }
+}
+</script>
---
+++ src/views/modules/etlscheduler/workflow/details/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <m-definition-details />
+</template>
+<script>
+import mDefinitionDetails from '@/views/modules/etlscheduler/dag/definitionDetails.vue'
+export default {
+  name: 'DefinitionDetailsIndex',
+  components: { mDefinitionDetails }
+}
+</script>
