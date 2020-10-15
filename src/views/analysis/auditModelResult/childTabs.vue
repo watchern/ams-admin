adf130af108328c682fc84e17b69971ac9ff1b55
liangrui <577055183@qq.com>
2020-10-15 14:39:56 +0800

    模型运行结果

--- src/views/analysis/auditModelResult/childTabs.vue
+++ src/views/analysis/auditModelResult/childTabs.vue
@@ -1,25 +1,25 @@
 <template>
   <!-- childTabs是子页签组件 -->
   <el-tabs type="border-card">
-    
-    <el-tab-pane label="主表"
-      ><childTabCon :nowtable="maintable" :modelUuid="modelUuid"></childTabCon
-    ></el-tab-pane>
+
     <el-tab-pane
-      :label="'辅表' + (key + 1)"
+      label="主表"
+    ><childTabCon :nowtable="maintable" :model-uuid="modelUuid" /></el-tab-pane>
+    <el-tab-pane
       v-for="(item, key) in helptables"
       :key="key"
-      ><childTabCon :nowtable="item"></childTabCon>
+      :label="'辅表' + (key + 1)"
+    ><childTabCon :nowtable="item" />
     </el-tab-pane>
   </el-tabs>
 </template>
 <script>
-import childTabCon from "@/views/analysis/auditModelResult/childTabCon";
-import { now } from "moment";
+import childTabCon from '@/views/analysis/auditModelResult/childTabCon'
+import { now } from 'moment'
 export default {
   components: {
-    childTabCon,
+    childTabCon
   },
-  props: ["maintable", "helptables","modelUuid"]
-};
+  props: ['maintable', 'helptables', 'modelUuid']
+}
 </script>
