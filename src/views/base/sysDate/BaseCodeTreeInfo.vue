90374856feae0df508ee08619e75371314f4088a
张学松 <zxs15075374774@163.com>
2020-10-20 15:16:29 +0800

    基础数据维护和调取接口

---
+++ src/views/base/sysDate/BaseCodeTreeInfo.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <div class="tree">
+      <BaseCodeTree />
+    </div>
+    <div class="divContent">
+      <BaseCodeTreeList ref="listData" />
+    </div>
+  </div>
+</template>
+
+<script>
+import BaseCodeTree from '@/views/base/sysDate/BaseCodeTree'
+import BaseCodeTreeList from '@/views/base/sysDate/BaseCodeTreeList'
+export default {
+  components: {
+    BaseCodeTree,
+    BaseCodeTreeList
+  },
+  mounted: function() {
+    this.$refs.listData.query()
+  }
+}
+</script>
+
+<style scoped>
+.tree {
+  width: 20%;
+  float: left;
+  margin-left: 1%;
+  height: 95%;
+}
+.divContent {
+  width: 78%;
+  float: right;
+}
+</style>
