64b3f334677498016929f62638abe6963e8c626c
张学松 <zxs15075374774@163.com>
2020-10-23 18:01:29 +0800

    数据角色

---
+++ src/api/data/role.js
@@ -0,0 +1,39 @@
+import request from '@/utils/request'
+
+const baseURL = '/amsdata'
+const controllerName = 'role'
+
+export function listByPage(data) {
+  return request({
+    baseURL: baseURL,
+    url: `/${controllerName}/pageList`,
+    method: 'post',
+    data
+  })
+}
+
+export function save(data) {
+  return request({
+    baseURL: baseURL,
+    url: `/${controllerName}/save`,
+    method: 'post',
+    data
+  })
+}
+
+export function update(data) {
+  return request({
+    baseURL: baseURL,
+    url: `/${controllerName}/update`,
+    method: 'put',
+    data
+  })
+}
+
+export function del(ids) {
+  return request({
+    baseURL: baseURL,
+    url: `/${controllerName}/delete/${ids}`,
+    method: 'delete'
+  })
+}
