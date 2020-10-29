7131cc6af7df86b6c343a5ca474fd445603aa2d1
duyiran <2192156580@qq.com>
2020-10-29 14:43:01 +0800

    数据源监控

--- src/views/modules/etlscheduler/etlconfigure/index.vue
+++ src/views/modules/etlscheduler/etlconfigure/index.vue
@@ -1,14 +1,14 @@
 <template>
   <el-tabs type="border-card" style="background:transparent!important;height:100%;">
-    <el-tab-pane label="数据源管理"><etl-data-resource /></el-tab-pane>
+    <el-tab-pane label="数据源管理"><etl-data-source /></el-tab-pane>
     <el-tab-pane label="文件目录管理">文件目录管理</el-tab-pane>
-    <el-tab-pane label="数据资源管理"><etl-data-source /></el-tab-pane>
+    <el-tab-pane label="数据资源管理"><etl-data-resource /></el-tab-pane>
     <el-tab-pane label="服务器管理">服务器管理</el-tab-pane>
   </el-tabs>
 </template>
 <script>
 import etlDataResource from '@/views/modules/etlscheduler/dataresource'
-import etlDataSource from '@/views/modules/etlscheduler/dataresource'
+import etlDataSource from '@/views/modules/etlscheduler/datasource'
 export default {
   components: { etlDataResource, etlDataSource }
 }
