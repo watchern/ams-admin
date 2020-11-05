87637b838eee6e250e630d4d20397d96cf98fcd4
liangrui <577055183@qq.com>
2020-11-05 11:12:13 +0800

    1.整理代码，加注释，排版和去掉console和debugger

--- src/api/analysis/auditmodelresult.js
+++ src/api/analysis/auditmodelresult.js
@@ -196,7 +196,9 @@ export function replaceParam(filterArr,arr,replaceSql){
     var moduleParamId = filterArr[j].moduleParamId
     for (var k = 0; k < arr.length; k++) { // 遍历当前节点绑定的参数
       if (arr[k].copyParamId === moduleParamId) {
-        replaceSql = replaceSql.replace(arr[k].id, filterArr[j].paramValue) // 将参数SQL中的参数ID替换为输入得值
+        if(typeof filterArr[j].paramValue != undefined){
+          replaceSql = replaceSql.replace(arr[k].id, filterArr[j].paramValue) // 将参数SQL中的参数ID替换为输入得值
+        }
       }
     }
   }
--- src/views/analysis/modelparam/crossrangeparam.vue
+++ src/views/analysis/modelparam/crossrangeparam.vue
@@ -1,26 +1,27 @@
 <template>
-      <div id="collapse">
-        <div :id="myId" style="padding: 10px 0;display: flex;flex-wrap: wrap" />
-      </div>
+  <div id="collapse">
+    <div :id="myId" style="padding: 10px 0; display: flex; flex-wrap: wrap" />
+  </div>
 </template>
 <script>
-import { initcrossrangeParamHtml } from '@/api/analysis/auditparam'
+import { initcrossrangeParamHtml } from "@/api/analysis/auditparam";
 export default {
   data() {
-    return {
-    }
+    return {};
   },
-  props:['myId']
-  ,
+  props: ["myId"], //传入的动态id
   methods: {
-    initParamHtmlSS(sql, paramsArr, name,id) {
-      initcrossrangeParamHtml(sql, paramsArr, name,id)
-    }
-  }
-}
+    /**
+     * 初始化横向参数界面
+     */
+    initParamHtmlSS(sql, paramsArr, name, id) {
+      initcrossrangeParamHtml(sql, paramsArr, name, id);
+    },
+  },
+};
 </script>
 <style>
-  body {
-    margin: 0;
-  }
+body {
+  margin: 0;
+}
 </style>
