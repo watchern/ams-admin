a30aeebe648702ade16801507bf79560d2134054
duyiran <2192156580@qq.com>
2020-11-14 15:58:05 +0800

    文件上传已存在文件

--- src/views/etlscheduler/filemanager/index.vue
+++ src/views/etlscheduler/filemanager/index.vue
@@ -280,15 +280,12 @@ export default {
       })
     },
     handleCheckChange(data, checked, indeterminate) {
-      console.log(data, checked, indeterminate)
-      console.log('data' + data.path)
-      console.log('check' + checked)
-      console.log('indeterminate' + indeterminate)
+      // console.log(data, checked, indeterminate)
       this.moveDirPath = data.path
       this.options.query.path = data.path
     },
     handleNodeClick(data, checked, node) {
-      console.log('多选框设为单选框', checked, data)
+      // console.log('多选框设为单选框', checked, data)
       if (checked) {
         this.$refs.treeForm.setCheckedKeys([data.id])
       }
