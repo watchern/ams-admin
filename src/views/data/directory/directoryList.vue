396e37b96ed13024bea442dbfc99a679b4c93d0d
张学松 <zxs15075374774@163.com>
2020-11-17 14:29:57 +0800

    修改前台样式

--- src/views/data/directory/directoryList.vue
+++ src/views/data/directory/directoryList.vue
@@ -1,47 +1,56 @@
 <template>
-  <div class="app-container">
+  <div class="page-container">
     <div class="filter-container">
       <QueryField
         ref="queryfield"
         :form-data="queryFields"
-        @submit="getList"
+        @submit="getListSelect"
       />
     </div>
-    <div>
-      <el-button type="danger" size="mini" :disabled="selections.length === 0" @click="handleDelete()">删除</el-button>
-      <el-button type="primary" size="mini" @click="handleCreate()">复制</el-button>
-      <el-button type="primary" size="mini" @click="handleCreate()">移动</el-button>
-      <el-button type="primary" size="mini" @click="handleCreate()">重命名</el-button>
-      <el-button type="primary" size="mini" @click="handleCreate()">新建表</el-button>
-      <el-button type="primary" size="mini" @click="handleCreate()">新建文件夹</el-button>
-      <el-button type="primary" size="mini" @click="handleCreate()">表结构维护</el-button>
-      <el-button type="primary" size="mini" @click="handleCreate()">表结构展示</el-button>
-      <el-button type="primary" size="mini" @click="handleCreate()">预览数据</el-button>
-    </div>
+    <el-row>
+      <el-col align="right">
+        <el-button type="primary" :disabled="selections.length === 0" title="删除" class="oper-btn delete" @click="delData()" />
+        <el-button type="primary" class="oper-btn iconoper-cancel" title="复制" @click="handleCreate()" />
+        <el-button type="primary" class="oper-btn iconoper-publish" title="移动" @click="handleCreate()" />
+        <el-button type="primary" class="oper-btn edit" title="重命名" @click="handleCreate()" />
+        <el-button type="primary" class="oper-btn add" title="新增表" @click="add" />
+        <el-button type="primary" class="oper-btn add-folder" title="新增文件夹" @click="add" />
+        <el-button type="primary" class="oper-btn  iconmenu-2" title="表结构维护" @click="add" />
+        <el-button type="primary" class="oper-btn  iconmenu-1" title="表结构展示" @click="add" />
+        <el-button type="primary" class="oper-btn iconoper-show" title="预览" @click="add" />
+      </el-col>
+    </el-row>
     <el-table
       :key="tableKey"
       v-loading="listLoading"
       :data="temp"
       border
+      fit
       highlight-current-row
       style="width: 100%;"
       @sort-change="sortChange"
       @selection-change="handleSelectionChange"
     >
       <el-table-column type="selection" width="55" />
-      <el-table-column label="名称" align="center" prop="label" />
-      <el-table-column label="资源类型" align="center" :formatter="formatTableType" prop="type" />
-      <el-table-column label="创建时间" align="center" :formatter="formatCreateTime" prop="createTime" />
-      <el-table-column label="大小" :formatter="formatTableSize" prop="tbSizeByte" />
+      <el-table-column label="名称" prop="label" />
+      <el-table-column label="资源类型" :formatter="formatTableType" prop="type" />
+      <el-table-column prop="createTime" label="创建时间" align="center">
+        <template slot-scope="scope">{{ formatCreateTime(scope.row.extMap.createTime) }}</template>
+      </el-table-column>
+      <el-table-column prop="tbSizeByte" label="大小" align="center">
+        <template slot-scope="scope">{{ formatTableSize(scope.row.extMap.tbSizeByte) }}</template>
+      </el-table-column>
     </el-table>
-    <!-- <pagination v-show="total>0" :total="total" :page.sync="pageQuery.pageNo" :limit.sync="pageQuery.pageSize" @pagination="getList" /> -->
+    <pagination v-show="total>0" :total="total" :page.sync="pageQuery.pageNo" :limit.sync="pageQuery.pageSize" @pagination="getListSelect" />
   </div>
 </template>
-
 <script>
+import Pagination from '@/components/Pagination' // secondary package based on el-pagination
 import QueryField from '@/components/Ace/query-field/index'
+import { getArrLength } from '@/utils'
+import { del } from '@/api/data/directory'
 export default {
-  components: { QueryField },
+  components: { Pagination, QueryField },
   filters: {
 
   },
@@ -56,12 +65,7 @@ export default {
       listLoading: false,
       // text 精确查询   fuzzyText 模糊查询  select下拉框  timePeriod时间区间
       queryFields: [
-        { label: '业务属性编码', name: 'attrCode', type: 'text', value: '' },
-        { label: '业务属性名称', name: 'attrName', type: 'fuzzyText' },
-        // {label: '性别', name:'sex', type: 'select',
-        //   data: [{name: '男', value: '1'}, {name: '女', value: '0'}],
-        //   default: '1'},
-        { label: '创建时间', name: 'createTime', type: 'timePeriod' }
+        { label: '名称', name: 'label', type: 'text', value: '' }
       ],
       pageQuery: {
         condition: null,
@@ -71,11 +75,15 @@ export default {
         sortName: 'create_time'
       },
       temp: [{
+        id: '',
         label: '',
         type: '',
-        createTime: '',
-        tbSizeByte: ''
+        extMap: {
+          createTime: '',
+          tbSizeByte: ''
+        }
       }],
+      allList: [],
       selections: [],
       dialogFormVisible: false,
       dialogStatus: '',
@@ -99,11 +107,13 @@ export default {
     formatTableType(row, column) {
       return row.type === 'table' ? '数据表' : '文件夹'
     },
-    formatTableSize(row, column) {
-      var limit = row.tbSizeByte
+    formatTableSize(limit) {
+      if (limit === undefined) {
+        limit = 0
+      }
       var size = ''
       if (limit < 1 * 1024) { // 小于1KB，则转化成B
-        size = limit.toFixed(2) + 'B'
+        size = parseFloat(limit).toFixed(2) + 'B'
       } else if (limit < 1 * 1024 * 1024) { // 小于1MB，则转化成KB
         size = (limit / 1024).toFixed(2) + 'KB'
       } else if (limit < 1 * 1024 * 1024 * 1024) { // 小于1GB，则转化成MB
@@ -119,10 +129,10 @@ export default {
       }
       return size
     },
-    formatCreateTime(row, column) {
+    formatCreateTime(createTime) {
       // 拼接日期规格为YYYY-MM-DD hh:mm:ss
-      if (row.createTime) {
-        var date = new Date(row.createTime)
+      if (createTime) {
+        var date = new Date(createTime)
         var Y = date.getFullYear()
         var M = date.getMonth() + 1
         var D = date.getDate()
@@ -149,16 +159,25 @@ export default {
         return ''
       }
     },
+    getListSelect(query) {
+      if (query.label) {
+        var list = this.allList.filter(obj => { return obj.label.indexOf(query.label) !== -1 })
+        this.total = getArrLength(list)
+        this.temp = list
+      } else {
+        this.temp = this.allList
+        this.total = getArrLength(this.temp)
+      }
+    },
     getList(data, node, tree) {
       var path = ''
       if (data.type === 'table') {
         this.temp = []
+        this.allList = []
         var tempObj = {}
-        tempObj.label = data.label
-        tempObj.type = data.type
-        tempObj.createTime = data.extMap.createTime
-        tempObj.tbSizeByte = data.extMap.tbSizeByte
+        tempObj = data
         this.temp.push(tempObj)
+        this.allList.push(tempObj)
         // 父节点node
         var newNode = node.parent
         while (newNode.parent != null) {
@@ -167,13 +186,37 @@ export default {
             newNode = newNode.parent
           }
         }
+        this.total = 1
       } else {
+        console.log(data.children)
         this.temp = data.children
+        this.allList = data.children
+        // eslint-disable-next-line no-undef
+        this.total = getArrLength(data.children)
+        this.getListSelect(null)
       }
     },
-    handleFilter() {
-      this.pageQuery.pageNo = 1
-      this.getList()
+    delData() {
+      var ids = []
+      this.selections.forEach((r, i) => {
+        if (r.type === 'fold') {
+          var foldObj = this.allList.filter(obj => { return obj.label === r.label })
+          var objTotal = getArrLength(foldObj[0].children)
+          if (objTotal > 0) {
+            ids.push(foldObj[0].label)
+          }
+        }
+      })
+      del(ids).then(() => {
+        this.getList()
+        this.$notify({
+          title: '成功',
+          message: '删除成功',
+          type: 'success',
+          duration: 2000,
+          position: 'bottom-right'
+        })
+      })
     },
     sortChange(data) {
       const { prop, order } = data
