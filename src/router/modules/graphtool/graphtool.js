26086143b9548a2fc0e7c6006c8efaa2119ff0c4
jinglei <614767437@qq.com>
2020-11-17 23:15:46 +0800

    增加图形化工具模块代码

---
+++ src/router/modules/graphtool/graphtool.js
@@ -0,0 +1,18 @@
+/** When your routing table is too long, you can split it into small modules **/
+
+const graphRouter = {
+  path: '/graphtool',
+  name: '图形化工具',
+  component: () => import('@/portal/default/index'),
+  meta: {
+  },
+  children: [
+    {
+      path: 'graphList',
+      name: '图形列表',
+      component: () => import('@/views/graphtool/index')
+    }
+  ]
+}
+
+export default graphRouter
