47331545251683299c64d575bb28aaec42d0fd22
jinglei <614767437@qq.com>
2020-11-18 15:32:37 +0800

    增加图形化工具模块代码

--- public/lib/graphtool/js/Init.js
+++ public/lib/graphtool/js/Init.js
@@ -9,12 +9,12 @@ window.MAX_AREA = window.MAX_AREA || 15000 * 15000;
 /*window.EXPORT_URL = window.EXPORT_URL || '/export';
 window.SAVE_URL = window.SAVE_URL || '/save';
 window.OPEN_URL = window.OPEN_URL || '/open';*/
-window.RESOURCES_PATH = (window.PURL || "") + '../graphtool/resources';
+window.RESOURCES_PATH = (window.PURL || "") + '../../lib/graphtool/resources';
 window.RESOURCE_BASE = window.RESOURCE_BASE || window.RESOURCES_PATH + '/grapheditor';
 window.STENCIL_PATH = window.STENCIL_PATH || 'framework/stencils';
-window.IMAGE_PATH = window.IMAGE_PATH || '../graphtool/images';
-window.STYLE_PATH = window.STYLE_PATH || '../graphtool/styles';
-window.CSS_PATH = window.CSS_PATH || '../graphtool/styles';
+window.IMAGE_PATH = window.IMAGE_PATH || '../../lib/graphtool/images';
+window.STYLE_PATH = window.STYLE_PATH || '../../lib/graphtool/styles';
+window.CSS_PATH = window.CSS_PATH || '../../lib/graphtool/styles';
 
 // Sets the base path, the UI language via URL param and configures the
 // supported languages to avoid 404s. The loading of all core language
