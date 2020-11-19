7e061c20e2b57afd2e15756bb8b2d72aac46d693
duyiran <2192156580@qq.com>
2020-11-19 13:00:13 +0800

    --

--- vue.config.js
+++ vue.config.js
@@ -64,7 +64,7 @@ module.exports = {
       // etl调度模块调用的地址
       '/etlscheduler': {
         // target: 'http://192.168.80.155:8080'
-        target: 'http://localhost:8082/amsetlscheduler',
+        target: 'http://localhost:1092/amsetlscheduler',
         // target: process.env.ETL_API_LOCATION,
         changeOrigin: true,
         pathRewrite: {
