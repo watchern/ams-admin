5d552bcc36dcbae05f82af69a7c0408e987b48c4
duyiran <2192156580@qq.com>
2020-11-25 16:54:22 +0800

    --

--- src/views/etlscheduler/etlconfigure/index.vue
+++ src/views/etlscheduler/etlconfigure/index.vue
@@ -1,24 +1,26 @@
 <template>
   <el-tabs type="border-card" style="background:transparent!important;height:100%;">
+    <el-tab-pane label="参数管理"><etl-param-field /></el-tab-pane>
     <el-tab-pane label="数据源管理"><etl-data-source /></el-tab-pane>
-    <el-tab-pane label="文件目录管理"><etl-file-directory /></el-tab-pane>
+    <!-- <el-tab-pane label="文件目录管理"><etl-file-directory /></el-tab-pane> -->
     <el-tab-pane label="数据资源管理"><etl-data-resource /></el-tab-pane>
     <!-- <el-tab-pane label="服务器管理">服务器管理</el-tab-pane> -->
     <!-- <el-tab-pane label="文件上传"><etl-file-upload /></el-tab-pane> -->
     <!-- <el-tab-pane label="hdfs文件管理"><etl-file-resource /></el-tab-pane> -->
-    <el-tab-pane label="文件管理"><etl-file-manager /></el-tab-pane>
+    <!-- <el-tab-pane label="文件管理"><etl-file-manager /></el-tab-pane> -->
   </el-tabs>
 </template>
 <script>
+import etlParamField from '@/views/etlscheduler/paramfield'
 import etlDataResource from '@/views/etlscheduler/etlconfigure/dataresource'
 import etlDataSource from '@/views/etlscheduler/etlconfigure/datasource'
-import etlFileDirectory from '@/views/etlscheduler/etlconfigure/filedirectory'
+// import etlFileDirectory from '@/views/etlscheduler/etlconfigure/filedirectory'
 // import etlFileResource from '@/views/etlscheduler/fileresource'
 // import etlFileUpload from '@/views/etlscheduler/upload'
-import etlFileManager from '@/views/etlscheduler/filemanager'
+// import etlFileManager from '@/views/etlscheduler/filemanager'
 export default {
-  // etlFileUpload, etlFileResource,
-  components: { etlDataResource, etlDataSource, etlFileDirectory, etlFileManager }
+  // , etlFileUpload, etlFileResource, etlFileDirectory, etlFileManager
+  components: { etlParamField, etlDataResource, etlDataSource }
 }
 </script>
 <style>
