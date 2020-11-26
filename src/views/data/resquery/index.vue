70725d09188516c4f05aa177d49599f1befe8ec7
张闯 <zhangchuang@chinasofti.com>
2020-11-26 15:26:32 +0800

    personuuid from cookies

--- src/views/data/resquery/index.vue
+++ src/views/data/resquery/index.vue
@@ -1,30 +1,24 @@
 <template>
   <div><data-tree @node-click="handleClick" v-if="personcode!==''"
-            :dataUserId="personcode"  :sceneCode="sceneCode"
+            :dataUserId="personCode"  :sceneCode="sceneCode"
   /></div>
 </template>
 
 <script>
 import dataTree from '@/views/data/role-res/data-tree'
-import { mapState } from 'vuex'
 import Cookies from 'js-cookie'
 
 export default {
   computed:{
-    //因为时序问题，store中没有personcode时组件可能被加载 导致dataUserId==''  所以要加personcode!=='' 控制
-    ...mapState({
-      personcode: state => state.user.code
-    })
-
   },
   components: { dataTree },
   data() {
     return {
-      sceneCode: 'auditor'
+      sceneCode: 'auditor',
+      personCode: Cookies.get("personcode")
     }
   },
   created() {
-    console.log(Cookies.get("personcode"));
   },
   methods: {
     handleClick(data, node, tree) {
