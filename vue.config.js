69a28edc08fa9310132145559fd2ea456d968179
dara <dara>
2021-01-12 17:05:01 +0800

    --

--- vue.config.js
+++ vue.config.js
@@ -235,6 +235,19 @@ module.exports = {
             })
           // https:// webpack.js.org/configuration/optimization/#optimizationruntimechunk
           config.optimization.runtimeChunk('single')
+
+          // const CompressionPlugin = require('compression-webpack-plugin')
+          // const productionGzipExtensions = /\.(js|css|json|txt|html|ico|svg)(\?.*)?$/i
+          // config.resolve.alias.set('@', resolve('src'))
+          // config.plugin('compressionPlugin')
+          //   .use(new CompressionPlugin({
+          //     filename: '[path].gz[query]',
+          //     algorithm: 'gzip',
+          //     test: productionGzipExtensions,
+          //     threshold: 10240,
+          //     minRatio: 0.8,
+          //     deleteOriginalAssets: false
+          //   }))
         }
       )
   }
