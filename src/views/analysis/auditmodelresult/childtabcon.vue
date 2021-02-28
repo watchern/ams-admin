897a36de784dee29c5656bda35b2e2fccacc90e7
zhangyu <929767636@qq.com>
2021-02-28 11:18:15 +0800

    1: sql编辑器样式改动

--- src/views/analysis/auditmodelresult/childtabcon.vue
+++ src/views/analysis/auditmodelresult/childtabcon.vue
@@ -1813,10 +1813,10 @@ export default {
 }
 
 >>>.el-btn-no-color{
-  width: 44px;
+  width: 40px;
   float: left;
   border: solid 1px #E0E0E0;
-  height: 29px;
+  height: 26px;
   margin: -7px 3px 3px 0px;
   cursor: pointer;
   text-align: center;
@@ -1824,10 +1824,10 @@ export default {
 
 >>>.el-btn-color{
   background: aliceblue;
-  width: 44px;
+  width: 40px;
   float: left;
   border: solid 1px #E0E0E0;
-  height: 29px;
+  height: 26px;
   margin: -7px 3px 3px 0px;
   cursor: pointer;
   text-align: center;
