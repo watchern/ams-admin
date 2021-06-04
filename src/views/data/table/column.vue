929e7bdf2dae9db99bfa32ca2d693f2cb29df49d
jiangwei <jiangwei004@chinasofti.com>
2021-06-04 14:48:45 +0800

    --

--- src/views/data/table/column.vue
+++ src/views/data/table/column.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="app-container" style="height:80vh;overflow:auto">
+  <div class="app-container">
     <el-row>
       <el-col v-if="openType !== 'showTable' && openType !== 'tableRegister'" align="right">
         <!-- <el-button type="primary" size="mini" class="oper-btn iconoper-export" title="上移" @click="upTheCol()" />
