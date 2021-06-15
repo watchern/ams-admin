b6c8f0ddb6d1ce1cd3613d26833b692764c6a9a9
luanwuxu <3379663227@qq.com>
2021-06-15 14:02:51 +0800

    删除意外提交

--- ams-admin-dev/src/mixin/table-common.js
+++
@@ -1,9 +0,0 @@
-export default {
-  data() {
-    return {
-      pageNum: 1,
-      pageSize: 20,
-      total: 120
-    }
-  }
-}
--- ams-admin-dev/src/store/modules/monitor/state.js
+++
@@ -1,7 +0,0 @@
-export default {
-  processStatus: null,
-  processStartTime: null,
-  processEndTime: null,
-  processGroupExecutionStatusType: null,
-  scheduleDetail: false
-}
