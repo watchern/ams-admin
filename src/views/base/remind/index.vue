79f8eeb157a1df76298710df1ed333953938eba3
duyiran <2192156580@qq.com>
2021-07-15 17:24:37 +0800

    --

--- src/views/base/remind/index.vue
+++ src/views/base/remind/index.vue
@@ -214,12 +214,12 @@ export default {
     },
     handdetails(data) {
       var id = data.remindUuid
-      if (data.modeUrl == null || data.modeUrl == '') {
-        this.temp = data
-        this.dialogFormVisible = true
-      } else {
-        this.selectDetail(data)
-      }
+      if (data.modeUrl == null || data.modeUrl == '') {
+        this.temp = data
+        this.dialogFormVisible = true
+      } else {
+        this.selectDetail(data)
+      }
       if (data.readStatus === 0) {
         updateRemind(id).then(result => {
           if (result.code === 0) {
