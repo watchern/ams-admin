0e8956c47e45bcf881a5d310d61d481431d41aaf
dara <dara>
2021-08-02 16:09:36 +0800

    工作流注释

--- src/views/flowwork/businessDetail.vue
+++ src/views/flowwork/businessDetail.vue
@@ -36,12 +36,12 @@
        * applyType:   审批单类型，工作流流程id(如：problemflow(问题复核)、clueflow(线索核实)、modelpublishflow(模型上线))
        * flowSetup:   工作流设置参数
        *               //控制审批页面元素显示隐藏参数
-                         flowSet:{
-                            opinionList:true,
-                            opinion:true,
-                            nextStep:true,     // 下一步
-                            done:false    // 是否已办
-                          },
+       *                  flowSet:{
+       *                     opinionList:true,
+       *                     opinion:true,
+       *                     nextStep:true,     // 下一步
+       *                     done:false    // 是否已办
+       *                   },
        * actionIdList: 业务操作ID,与工作流无关的其他操作
        */
       props: ["versionUuid", "appDataUuid","applyType","flowSetup","actionIdList"],
