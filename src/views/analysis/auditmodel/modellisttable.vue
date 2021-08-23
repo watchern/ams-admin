5ca2ed764df8142b9080c5e4a5a11d8efe211da6
peizhaojie <peizhaojie@chinasofti.com>
2021-08-23 18:56:33 +0800

    更改58数据库与72数据库判断管理员方法

--- src/views/analysis/auditmodel/modellisttable.vue
+++ src/views/analysis/auditmodel/modellisttable.vue
@@ -548,10 +548,13 @@ export default {
         const newArray = dataArray[0].roleId;
         const sysRole = "系统管理员";
         getSystemRole(sysRole).then((re) => {
-          if(newArray[0] == re.data.roleid ){
+          console.log("2222222222222")
+          if(newArray[0] == re.data.roleId ){
+            console.log("3333333333333")
             this.ifmanger = 1
             this.jinyong = 0
           }else{
+            console.log(newArray[0])
             this.ifmanger = 0
             this.jinyong = 1
             this.btnState = {
