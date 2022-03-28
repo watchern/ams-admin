04786a35d94b5f52806da79fdab7ffdfe3ebcb4e
luanwuxu <3379663227@qq.com>
2022-03-28 10:17:02 +0800

    兼容了一半

--- vue.config.js
+++ vue.config.js
@@ -32,7 +32,7 @@ module.exports = {
    * In most cases please use '/' !!!
    * Detail: https://cli.vuejs.org/config/#publicpath
    */
-  publicPath: './',
+   publicPath: './',
   outputDir: 'dist',
   assetsDir: 'static',
   lintOnSave: false,
@@ -187,88 +187,145 @@ module.exports = {
       //   }
       // },
     }
+  }
+  ,
+  pluginOptions: {
+    'style-resources-loader': {
+      preProcessor: 'stylus',
+      patterns: []
+    }
   },
+  transpileDependencies: [
+    "element-ui",
+    "@vue/composition-api",
+    "@jiaminghi",
+    "ag-grid",
+    "ag-grid-community",
+    "ag-grid-vue",
+    "ams-clue-vue",
+    "ams-datamax",
+    "ams-etlscheduler-ui",
+    "ams-file-vue",
+    "ams-starflow-vue",
+    "axios",
+    "babel-preset-es2015",
+    "classlist-polyfill",
+    "codemirror",
+    "dropzone",
+    "echarts",
+    "jquery",
+    "js-cookie",
+    "jszip",
+    "leancloud-storage",
+    "moment",
+    "node-sass",
+    "nprogress",
+    "postcss-loader",
+    "regenerator-runtime",
+    "sass",
+    "sdmobile-vue",
+    "spark-md5",
+    "stylus",
+    "stylus-loader",
+    "svg-sprite-loader",
+    "swiper",
+    "v-runtime-template",
+    "vue-animate-number",
+    "vue-awesome-swiper",
+    "vue-class-component",
+    "vue-echarts",
+    "vue-grid-layout",
+    "vue-i18n",
+    "vue-json-excel",
+    "vue-property-decorator",
+    "vue-router",
+    "vue-simple-uploader",
+    "vuex",
+    "wangeditor",
+    "webpack-dev-server",
+    "xlsx",
+    "zrender"],
   configureWebpack: config => {
     // provide the app's title in webpack's name field, so that
     // it can be accessed in index.html to inject the correct title.
     // Object.assign(config, {
-    config.entry.app = ["@babel/polyfill", "./src/main.js"]
-    return{
+    config.entry.app = ["./src/main.js"]
+    return {
       output: { // 输出重构  打包编译后的 文件名称  【模块名称.版本号.时间戳】
         filename: `static/js/[name].${Timestamp}.js`,
         chunkFilename: `static/js/[name].${Timestamp}.js`
       },
-    name: name,
-    resolve: {
-      alias: {
-        '@': resolve('src'),
-        '@ETL': 'ams-etlscheduler-ui/src',
+      name: name,
+      resolve: {
+        alias: {
+          '@': resolve('src'),
+          '@ETL': 'ams-etlscheduler-ui/src',
         '@MAX': 'ams-datamax/src'
-      }
-    },
-    // gzip压缩
-    plugins: [
-      // const productionGzipExtensions = /\.(js|css|json|txt|html|ico|svg)(\?.*)?$/i
-      // config.resolve.alias.set('@', resolve('src'))
-      // config.plugin('compressionPlugin')
-      //   .use(new CompressionPlugin({
-      //     filename: '[path].gz[query]',
-      //     algorithm: 'gzip',
-      //     test: productionGzipExtensions,
-      //     threshold: 10240,
-      //     minRatio: 0.8,
-      //     deleteOriginalAssets: false
-      //   }))
-      new CompressionWebpackPlugin({
-        filename: '[path].gz[query]',
-        algorithm: 'gzip',
-        test: /\.(js|css|json|txt|html|ico|svg)(\?.*)?$/i,
-        threshold: 10240, // 对超过10k的数据进行压缩
-        minRatio: 0.8, // 只有压缩率小于这个值的资源才会被处理
-        deleteOriginalAssets: false // 删除原文件
-        // deleteOriginalAssets: process.env.NODE_ENV !== 'development' // 删除原文件
-      })
-      // ,
-      // new ProvidePlugin({
-      //   _: 'lodash'
-      // })
-      // ,
-      // new UglifyJsPlugin({
-      //     uglifyOptions: {
-      //       //生产环境自动删除console
-      //       compress: {
-      //         // warnings: false, // 若打包错误，则注释这行
-      //         drop_debugger: true,
-      //         drop_console: true,
-      //         pure_funcs: ['console.log']
-      //       }
-      //     },
-      //     sourceMap: false,
-      //     parallel: true
-      //   })
-    ]
+        }
+      },
+      // gzip压缩
+      plugins: [
+        // const productionGzipExtensions = /\.(js|css|json|txt|html|ico|svg)(\?.*)?$/i
+        // config.resolve.alias.set('@', resolve('src'))
+        // config.plugin('compressionPlugin')
+        //   .use(new CompressionPlugin({
+        //     filename: '[path].gz[query]',
+        //     algorithm: 'gzip',
+        //     test: productionGzipExtensions,
+        //     threshold: 10240,
+        //     minRatio: 0.8,
+        //     deleteOriginalAssets: false
+        //   }))
+        new CompressionWebpackPlugin({
+          filename: '[path].gz[query]',
+          algorithm: 'gzip',
+          test: /\.(js|css|json|txt|html|ico|svg)(\?.*)?$/i,
+          threshold: 10240, // 对超过10k的数据进行压缩
+          minRatio: 0.8, // 只有压缩率小于这个值的资源才会被处理
+          deleteOriginalAssets: false // 删除原文件
+          // deleteOriginalAssets: process.env.NODE_ENV !== 'development' // 删除原文件
+        })
+        // ,
+        // new ProvidePlugin({
+        //   _: 'lodash'
+        // })
+        // ,
+        // new UglifyJsPlugin({
+        //     uglifyOptions: {
+        //       //生产环境自动删除console
+        //       compress: {
+        //         // warnings: false, // 若打包错误，则注释这行
+        //         drop_debugger: true,
+        //         drop_console: true,
+        //         pure_funcs: ['console.log']
+        //       }
+        //     },
+        //     sourceMap: false,
+        //     parallel: true
+        //   })
+      ]
     }
   },
-  chainWebpack:config =>{
+  chainWebpack: config => {
     // it can improve the speed of the first screen, it is recommended to turn on preload
     // it can improve the speed of the first screen, it is recommended to turn on preload
     //兼容ie11
-    config.module
-        .rule('view-design')
-        .test(/view-design.src.*?js$/)
-        .use('babel')
-        .loader('babel-loader')
-        .end()
-    config.plugin('preload').tap(() => [{
-      rel: 'preload',
-      // to ignore runtime.js
-      // https://github.com/vuejs/vue-cli/blob/dev/packages/@vue/cli-service/lib/config/app.js#L171
-      fileBlacklist: [/\.map$/, /hot-update\.js$/, /runtime\..*\.js$/],
-      include: 'initial'
-    }])
+    // config.module
+    //   .rule('view-design')
+    //   .test(/view-design.src.*?js$/)
+    //   .use('babel')
+    //   .loader('babel-loader')
+    //   .end()
+    // config.plugin('preload').tap(() => [{
+    //   rel: 'preload',
+    //   // to ignore runtime.js
+    //   // https://github.com/vuejs/vue-cli/blob/dev/packages/@vue/cli-service/lib/config/app.js#L171
+    //   fileBlacklist: [/\.map$/, /hot-update\.js$/, /runtime\..*\.js$/],
+    //   include: 'initial'
+    // }])
 
     // when there are many pages, it will cause too many meaningless requests
-    config.plugins.delete('prefetch')
+    // config.plugins.delete('prefetch')
 
     // set svg-sprite-loader
     config.module
@@ -299,33 +356,39 @@ module.exports = {
             .end()
           config
             .optimization.splitChunks({
-            chunks: 'all',
-            cacheGroups: {
-              libs: {
-                name: 'chunk-libs',
-                test: /[\\/]node_modules[\\/]/,
-                priority: 10,
-                chunks: 'initial' // only package third parties that are initially dependent
-              },
-              elementUI: {
-                name: 'chunk-elementUI', // split elementUI into a single package
-                priority: 20, // the weight needs to be larger than libs and app or it will be packaged into libs or app
-                test: /[\\/]node_modules[\\/]_?element-ui(.*)/ // in order to adapt to cnpm
-              },
-              commons: {
-                name: 'chunk-commons',
-                test: resolve('src/components'), // can customize your rules
-                minChunks: 3, //  minimum common number
-                priority: 5,
-                reuseExistingChunk: true
+              chunks: 'all',
+              cacheGroups: {
+                libs: {
+                  name: 'chunk-libs',
+                  test: /[\\/]node_modules[\\/]/,
+                  priority: 10,
+                  chunks: 'initial' // only package third parties that are initially dependent
+                },
+                elementUI: {
+                  name: 'chunk-elementUI', // split elementUI into a single package
+                  priority: 20, // the weight needs to be larger than libs and app or it will be packaged into libs or app
+                  test: /[\\/]node_modules[\\/]_?element-ui(.*)/ // in order to adapt to cnpm
+                },
+                amsEtlschedulerUi: {
+                  name: 'chunk-elementUI', // split elementUI into a single package
+                  priority: 20, // the weight needs to be larger than libs and app or it will be packaged into libs or app
+                  test: /[\\/]node_modules[\\/]_?ams-etlscheduler-ui(.*)/ // in order to adapt to cnpm
+                },
+                commons: {
+                  name: 'chunk-commons',
+                  test: resolve('src/components'), // can customize your rules
+                  minChunks: 3, //  minimum common number
+                  priority: 5,
+                  reuseExistingChunk: true
+                }
               }
-            }
-          })
+            })
           // https:// webpack.js.org/configuration/optimization/#optimizationruntimechunk
           config.optimization.runtimeChunk('single')
         }
       )
-  },
+  }
+  ,
   css: {
     loaderOptions: {
       sass: {
