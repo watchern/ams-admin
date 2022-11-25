f9ead65e6ca5871d0eba1109df3c184e530988fb
lihongguang <837096mm>
2022-11-25 11:23:00 +0800

    11-25新提交

--- src/views/data/tblrel/index.vue
+++ src/views/data/tblrel/index.vue
@@ -1,158 +1,146 @@
 <template>
   <div class="page-container">
     <div class="filter-container">
-      <QueryField ref="queryfield" :form-data="queryFields" @submit="getList" />
+      <QueryField ref="queryfield"
+                  :form-data="queryFields"
+                  @submit="getList" />
     </div>
     <el-row>
       <el-col align="right">
-        <el-button type="primary" class="oper-btn add" @click="add" />
-        <el-button
-          type="primary"
-          class="oper-btn edit"
-          :disabled="selections.length !== 1"
-          @click="update"
-        />
-        <el-button
-          type="primary"
-          class="oper-btn delete"
-          :disabled="selections.length === 0"
-          @click="deleteRel"
-        />
+        <el-button type="primary"
+                   class="oper-btn add"
+                   @click="add" />
+        <el-button type="primary"
+                   class="oper-btn edit"
+                   :disabled="selections.length !== 1"
+                   @click="update" />
+        <el-button type="primary"
+                   class="oper-btn delete"
+                   :disabled="selections.length === 0"
+                   @click="deleteRel" />
       </el-col>
     </el-row>
-    <el-table
-      :key="tableKey"
-      v-loading="listLoading"
-      :data="list"
-      border
-      fit
-      highlight-current-row
-      style="width: 100%"
-      @sort-change="sortChange"
-      @selection-change="handleSelectionChange"
-    >
-      <el-table-column type="selection" width="55" />
-      <el-table-column label="表名称" prop="tbName" />
-      <el-table-column label="字段名称" prop="colName" />
-      <el-table-column label="从表名称" prop="relationTableName" />
-      <el-table-column label="从表字段" prop="relationCol" />
-      <el-table-column
-        label="关联关系"
-        align="center"
-        prop="sqlGenJoinType"
-        :formatter="formatSqlGenJoinType"
-      />
+    <el-table :key="tableKey"
+              v-loading="listLoading"
+              :data="list"
+              border
+              fit
+              highlight-current-row
+              style="width: 100%"
+              @sort-change="sortChange"
+              @selection-change="handleSelectionChange">
+      <el-table-column type="selection"
+                       width="55" />
+      <el-table-column label="表名称"
+                       prop="tbName" />
+      <el-table-column label="字段名称"
+                       prop="colName" />
+      <el-table-column label="从表名称"
+                       prop="relationTableName" />
+      <el-table-column label="从表字段"
+                       prop="relationCol" />
+      <el-table-column label="关联关系"
+                       align="center"
+                       prop="sqlGenJoinType"
+                       :formatter="formatSqlGenJoinType" />
     </el-table>
-    <pagination
-      v-show="total > 0"
-      :total="total"
-      :page.sync="pageQuery.pageNo"
-      :limit.sync="pageQuery.pageSize"
-      @pagination="getList"
-    />
+    <pagination v-show="total > 0"
+                :total="total"
+                :page.sync="pageQuery.pageNo"
+                :limit.sync="pageQuery.pageSize"
+                @pagination="getList" />
 
-    <el-dialog :title="textMap[dialogStatus]" :visible.sync="dialogFormVisible">
+    <el-dialog :title="textMap[dialogStatus]"
+               :visible.sync="dialogFormVisible">
       <template>
-        <el-form
-          ref="dataForm"
-          :rules="rules"
-          :model="temp"
-          label-position="right"
-          width="80%"
-          class="detail-form"
-        >
-          <el-form-item prop="tableMetaUuid" label="表名称">
-            <el-input v-model="temp.tbName" :disabled="true" />
-            <el-input
-              v-model="temp.tableMetaUuid"
-              :disabled="true"
-              style="display: none"
-            />
+        <el-form ref="dataForm"
+                 :rules="rules"
+                 :model="temp"
+                 label-position="right"
+                 width="80%"
+                 class="detail-form">
+          <el-form-item prop="tableMetaUuid"
+                        label="表名称">
+            <el-input v-model="temp.tbName"
+                      :disabled="true" />
+            <el-input v-model="temp.tableMetaUuid"
+                      :disabled="true"
+                      style="display: none" />
           </el-form-item>
-          <el-form-item prop="colMetaUuid" label="字段名称">
+          <el-form-item prop="colMetaUuid"
+                        label="字段名称">
             <el-row>
               <el-col :span="22">
-                <el-input v-model="temp.colName" :disabled="true" />
-                <el-input
-                  v-model="temp.colMetaUuid"
-                  :disabled="true"
-                  style="display: none"
-                />
+                <el-input v-model="temp.colName"
+                          :disabled="true" />
+                <el-input v-model="temp.colMetaUuid"
+                          :disabled="true"
+                          style="display: none" />
               </el-col>
               <el-col :span="2">
                 <el-button @click="showDataTree(1)">选择</el-button>
               </el-col>
             </el-row>
           </el-form-item>
-          <el-form-item prop="relTableMeataUuid" label="从表名称">
-            <el-input v-model="temp.relationTableName" :disabled="true" />
-            <el-input
-              v-model="temp.relTableMetaUuid"
-              :disabled="true"
-              style="display: none"
-            />
+          <el-form-item prop="relTableMeataUuid"
+                        label="从表名称">
+            <el-input v-model="temp.relationTableName"
+                      :disabled="true" />
+            <el-input v-model="temp.relTableMetaUuid"
+                      :disabled="true"
+                      style="display: none" />
           </el-form-item>
-          <el-form-item prop="relColMetaUuid" label="从表字段">
+          <el-form-item prop="relColMetaUuid"
+                        label="从表字段">
             <el-row>
               <el-col :span="22">
-                <el-input v-model="temp.relationCol" :disabled="true" />
-                <el-input
-                  v-model="temp.relColMetaUuid"
-                  style="display: none"
-                  hidden
-                  :disabled="true"
-                />
+                <el-input v-model="temp.relationCol"
+                          :disabled="true" />
+                <el-input v-model="temp.relColMetaUuid"
+                          style="display: none"
+                          hidden
+                          :disabled="true" />
               </el-col>
               <el-col :span="2">
                 <el-button @click="showDataTree(2)">选择</el-button>
               </el-col>
             </el-row>
           </el-form-item>
-          <el-form-item prop="sqlGenJoinType" label="关联关系" >
-            <el-select
-              v-model="temp.sqlGenJoinType"
-              filterable
-              placeholder="请选择关联关系"
-              style="width: 100%"
-            >
-              <el-option
-                v-for="item in sqlJoinCols"
-                :key="item.sqlGenJoinType"
-                :label="item.sqlGenJoinName"
-                :value="item.sqlGenJoinType"
-                :disabled="item.disabled"
-              />
+          <el-form-item prop="sqlGenJoinType"
+                        label="关联关系">
+            <el-select v-model="temp.sqlGenJoinType"
+                       filterable
+                       placeholder="请选择关联关系"
+                       style="width: 100%">
+              <el-option v-for="item in sqlJoinCols"
+                         :key="item.sqlGenJoinType"
+                         :label="item.sqlGenJoinName"
+                         :value="item.sqlGenJoinType"
+                         :disabled="item.disabled" />
             </el-select>
           </el-form-item>
         </el-form>
       </template>
       <div slot="footer">
-        <el-button size="mini" @click="dialogFormVisible = false"
-          >取消</el-button
-        >
-        <el-button
-          type="primary"
-          @click="dialogStatus === 'create' ? createData() : updateData()"
-          >保存</el-button
-        >
+        <el-button size="mini"
+                   @click="dialogFormVisible = false">取消</el-button>
+        <el-button type="primary"
+                   @click="dialogStatus === 'create' ? createData() : updateData()">保存</el-button>
       </div>
-      <el-dialog
-        v-if="dataTableTree"
-        class="abow_dialog"
-        :destroy-on-close="true"
-        :append-to-body="true"
-        :visible.sync="dataTableTree"
-        title="请选择数据表"
-        width="600px"
-      >
-        <data-tree
-          ref="dataTableTree"
-          :data-user-id="dataUserId"
-          :scene-code="sceneCode"
-        />
+      <el-dialog v-if="dataTableTree"
+                 class="abow_dialog"
+                 :destroy-on-close="true"
+                 :append-to-body="true"
+                 :visible.sync="dataTableTree"
+                 title="请选择数据表"
+                 width="600px">
+        <data-tree ref="dataTableTree"
+                   :data-user-id="dataUserId"
+                   :scene-code="sceneCode" />
         <div slot="footer">
           <el-button @click="dataTableTree = false">取消</el-button>
-          <el-button type="primary" @click="getDataTable">确定</el-button>
+          <el-button type="primary"
+                     @click="getDataTable">确定</el-button>
         </div>
       </el-dialog>
     </el-dialog>
@@ -160,7 +148,7 @@
 </template>
 
 <script>
-import dataTree from "@/views/data/role-res/data-tree";
+import dataTree from "@/views/data/role-res/data-tree_copy";
 import Pagination from "@/components/Pagination"; // secondary package based on el-pagination
 import { getTableByCol } from "@/api/data/table-info";
 import { listByPage, save, update, del } from "@/api/data/tablerelation";
@@ -168,7 +156,7 @@ import QueryField from "@/components/public/query-field/index";
 
 export default {
   components: { Pagination, QueryField, dataTree },
-  data() {
+  data () {
     return {
       tableKey: "tableRelationUuid",
       list: null,
@@ -239,11 +227,11 @@ export default {
       downloadLoading: false,
     };
   },
-  created() {
+  created () {
     this.getList();
   },
   methods: {
-    getList(query) {
+    getList (query) {
       this.listLoading = true;
       if (query) this.pageQuery.condition = query;
       listByPage(this.pageQuery).then((resp) => {
@@ -252,7 +240,7 @@ export default {
         this.listLoading = false;
       });
     },
-    showDataTree(val) {
+    showDataTree (val) {
       this.selectType = val;
       this.dataTableTree = true;
       let self = this;
@@ -271,7 +259,7 @@ export default {
       //     : ""; // 子级递归
       // });
     },
-    changeTreeNodeStatus(val, flag) {
+    changeTreeNodeStatus (val, flag) {
       let _this = this;
       setTimeout(function () {
         const node = val;
@@ -287,17 +275,17 @@ export default {
         }
       }, 500);
     },
-    handleFilter() {
+    handleFilter () {
       this.pageQuery.pageNo = 1;
       this.getList();
     },
-    sortChange(data) {
+    sortChange (data) {
       const { prop, order } = data;
       this.pageQuery.sortBy = order;
       this.pageQuery.sortName = prop;
       this.handleFilter();
     },
-    resetTemp() {
+    resetTemp () {
       this.temp = {
         tableMetaUuid: "",
         tbName: "",
@@ -310,7 +298,7 @@ export default {
         sqlGenJoinType: "",
       };
     },
-    add() {
+    add () {
       this.resetTemp();
       this.dialogStatus = "create";
       this.dialogFormVisible = true;
@@ -318,7 +306,7 @@ export default {
         this.$refs["dataForm"].clearValidate();
       });
     },
-    createData() {
+    createData () {
       this.$refs["dataForm"].validate((valid) => {
         if (valid) {
           save(this.temp).then(() => {
@@ -335,7 +323,7 @@ export default {
         }
       });
     },
-    update() {
+    update () {
       this.temp = Object.assign({}, this.selections[0]); // copy obj
       this.dialogStatus = "update";
       this.dialogFormVisible = true;
@@ -343,7 +331,7 @@ export default {
         this.$refs["dataForm"].clearValidate();
       });
     },
-    updateData() {
+    updateData () {
       this.$refs["dataForm"].validate((valid) => {
         if (valid) {
           const tempData = Object.assign({}, this.temp);
@@ -364,7 +352,7 @@ export default {
         }
       });
     },
-    deleteRel() {
+    deleteRel () {
       var ids = [];
       this.selections.forEach((r, i) => {
         ids.push(r.tableRelationUuid);
@@ -387,7 +375,7 @@ export default {
         });
       });
     },
-    formatSqlGenJoinType(row) {
+    formatSqlGenJoinType (row) {
       let sqlGenJoinType = "";
       switch (row.sqlGenJoinType) {
         case 1:
@@ -405,7 +393,7 @@ export default {
       }
       return sqlGenJoinType;
     },
-    getDataTable() {
+    getDataTable () {
       const dataTree = this.$refs.dataTableTree.getTree();
       const currentNode = dataTree.getCurrentNode();
       if (currentNode.type !== "col") {
@@ -430,7 +418,7 @@ export default {
       }
       this.dataTableTree = false;
     },
-    loadTable(id) {
+    loadTable (id) {
       // 获取选中列的表id，表名称信息
       getTableByCol(id).then((result) => {
         if (result.data == null) {
@@ -444,7 +432,7 @@ export default {
         }
       });
     },
-    handleSelectionChange(val) {
+    handleSelectionChange (val) {
       this.selections = val;
     },
     getSortClass: function (key) {
@@ -456,8 +444,8 @@ export default {
 </script>
 
 <style>
-  .abow_dialog .el-dialog .el-dialog__body .app-container{
-    height:70vh!important;
-    width:550px!important;
-  }
+.abow_dialog .el-dialog .el-dialog__body .app-container {
+  height: 70vh !important;
+  width: 550px !important;
+}
 </style>
