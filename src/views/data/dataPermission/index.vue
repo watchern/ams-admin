bde3f62d04449bc0353a7828cee23cb932e194d0
于世杰 <yusj@devops.com>
2022-12-20 10:44:53 +0800

    添加数据权限页面

---
+++ src/views/data/dataPermission/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>111</div>
+</template>
+
+<script>
+export default {
+  name: "dataPermission",
+  components: {},
+  props: [],
+  data() {
+    return {};
+  },
+  computed: {},
+  watch: {},
+  mounted() {},
+  methods: {}
+};
+</script>
+
+<style lang="scss" scoped></style>
