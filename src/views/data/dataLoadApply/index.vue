41e580121db11845506706dacec9bb2ae4270fcf
周志豪 <2311364792@qq.com>
2023-01-09 15:51:49 +0800

    数据装载与下线页面，选择多个文件上传前端代码优化

--- src/views/data/dataLoadApply/index.vue
+++ src/views/data/dataLoadApply/index.vue
@@ -1056,11 +1056,6 @@
                                     applyPerson: this.form.applyPerson,
                                     operationType: this.form.operationType,
                                     loadType: this.form.loadType,
-                                    fileType: this.fileType,
-                                    lineSeparator: this.form.lineSeparator,
-                                    columnSeparator: this.form.columnSeparator,
-                                    isHeaderLine: this.form.isHeaderLine,
-                                    fileName: this.form.fileName,
                                 },
                                 "tableData": JSON.stringify(this.tableData),
                             }
@@ -1142,6 +1137,8 @@
             handleClose(form) {
                 this.$refs[form].resetFields() //清空添加的值
                 this.updateShow = false
+                this.fileList = []
+                this.tableData = []
             },
             //子组件fileupload传值给父组件，用showFileType事件接收值赋给变量fileType
             showFileType(fileType) {
