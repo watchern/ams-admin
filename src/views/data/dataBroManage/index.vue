bcbe68489f6a0a0c9acf40d86e0865e2cbe1345c
tianhao <@tianhao-etc>
2023-01-28 15:50:50 +0800

    修改页面样式（暂时提交）

--- src/views/data/dataBroManage/index.vue
+++ src/views/data/dataBroManage/index.vue
@@ -5,7 +5,7 @@
       <LeftTrees ref="tree_left" @details="Details"></LeftTrees>
     </div>
     <div class="right_conter padding10">
-      <div style="padding: 0 40px" class="mose">
+      <div class="mose">
         <!-- 数据表关联关系 -->
         <ProcessTree
           :tableMetaUuid="tableMetaUuid"
@@ -21,62 +21,68 @@ import LeftTrees from "@/components/loginTree/leftTree.vue";
 import ProcessTree from "@/components/directory/processTree.vue";
 
 export default {
-  components: { LeftTrees, ProcessTree },
-  props: {
-    tableMetaUuid: {
-      type: String,
-      default() {
-        return "";
-      },
+    components: { LeftTrees, ProcessTree },
+    props: {
+        tableMetaUuid: {
+            type: String,
+            default() {
+                return "";
+            },
+        },
     },
-  },
-  data() {
-    return {};
-  },
-  computed: {},
-  watch: {},
-  mounted() {
-    // this.details(this.tableMetaUuid);
-  },
-  created() {},
-  methods: {
-    Details(tableMetaUuid) {
-      this.tableMetaUuid = tableMetaUuid;
-      this.$nextTick(() => {
-        this.$refs.tableLines.init(1); //刷新列表 更新关系树
-      });
+    data() {
+        return {}
     },
-  },
-};
+    computed: {},
+    watch: {},
+    mounted() {
+        // this.details(this.tableMetaUuid);
+    },
+    created() {
+    },
+    methods: {
+        Details(tableMetaUuid) {
+            this.tableMetaUuid = tableMetaUuid
+            this.$nextTick(() => {
+                this.$refs.tableLines.init(1)//刷新列表 更新关系树
+            })
+        },
+    },
+}
 </script>
 
 <style scoped>
-.page-container {
-  min-width: 1300px;
-  display: flex;
-  padding: 0px !important;
-}
+    .page-container {
+        display: flex;
+        padding: 0px !important;
+    }
 
-.left_conter {
-  height: calc(100vh - 130px);
-  position: relative;
-  width: 280px;
-  padding-right: 40px;
-}
+    .left_conter {
+        height: calc(100vh - 123px);
+        width: 20%;
+        position: relative;
+        padding-right: 20px;
+    }
 
-.right_conter {
-  min-width: 1500px;
-  overflow: auto;
-  height: calc(100vh - 150px);
-}
+    .left_conter >>> .conter_vh {
+        height: calc(100% - 143px) !important;
+    }
 
-.mose >>> myDiagramDiv {
-  height: 1500px !important;
-  border: 1px solid;
-}
+    .right_conter {
+        overflow: auto;
+        /*min-width: calc(100vw - 380px);*/
+        width: calc(100vw - 150px);
+        height: calc(100vh - 150px);
+    }
 
-.mose {
-  padding: 0 40px;
-  position: relative;
-}
+    .mose {
+        padding: 0 30px;
+        position: relative;
+    }
+
+    .mose ::v-deep #myDiagramDiv {
+      width: 100%;
+      height: calc(100vh - 150px);
+      border: 1px solid;
+    }
 </style>
