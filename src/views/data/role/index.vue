1e7bccd6c110288adb6a1d578a34aa69390d95fe
lwk36 <912079367@qq.com>
2023-02-21 17:49:29 +0800

    数据授权bug

--- src/views/data/role/index.vue
+++ src/views/data/role/index.vue
@@ -257,7 +257,10 @@ export default {
         this.authenTypeJson = getDictList('004001')
       }
       this.listLoading = true
-      if (query) this.pageQuery.condition = query
+      if (query) {
+        this.pageQuery.condition = query;
+        this.pageQuery.pageNo = 1;
+      }
       listByPage(this.pageQuery).then(resp => {
         this.total = resp.data.total
         this.list = resp.data.records
