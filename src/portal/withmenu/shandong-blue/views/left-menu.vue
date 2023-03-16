649333df15ec699ad28b89b8b7bf53508a643144
于世杰 <yusj@devops.com>
2023-03-16 08:30:34 +0800

    去掉修改密码

--- src/portal/withmenu/shandong-blue/views/left-menu.vue
+++ src/portal/withmenu/shandong-blue/views/left-menu.vue
@@ -209,11 +209,11 @@ export default {
           name: "帮助",
           method: this.showHelp,
         },
-        {
-          icon: "",
-          name: "修改密码",
-          method: this.goresetpassword,
-        },
+        // {
+        //   icon: "",
+        //   name: "修改密码",
+        //   method: this.goresetpassword,
+        // },
         {
           icon: "",
           name: "退出",
